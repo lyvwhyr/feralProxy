18e360334ee51e7b272988a5952e1e17c00250c8
brandon moody <brandon@thef33d.com>
2017-12-04 02:50:49 -0800

    basic proxy working

--- src/main.go
+++ src/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/elazarl/goproxy"
 )
 
-func main() {
-	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+func getURLResponse(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
 
-	proxy.OnRequest().DoFunc(
-		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			r.Header.Set("Access-Control-Allow-Origin", "*")
-			return r, nil
-		})
+	return resp, nil
+}
+
+func onRequest(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+	resp, err := getURLResponse(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	responseBodyString, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(responseBodyString)
+	w.WriteHeader(200)
+
+}
+
+func main() {
+	http.HandleFunc("/", onRequest)          // set router
+	err := http.ListenAndServe(":8080", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 
 }
