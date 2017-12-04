9e7fb0af731433eb78669d44e790e836fc84574f
brandon moody <brandon@thef33d.com>
2017-12-04 01:27:44 -0800

    add vscode settings and deps lock updaate

--- src/main.go
+++ src/main.go
@@ -11,10 +11,11 @@ func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(":8080", proxy))
-}
 
-proxy.OnRequest().DoFunc(
-	func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
-			r.Header.Set("Access-Control-Allow-Origin","*")
-			return r,nil
-	})
+	proxy.OnRequest().DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			r.Header.Set("Access-Control-Allow-Origin", "*")
+			return r, nil
+		})
+
+}
